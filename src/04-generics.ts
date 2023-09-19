7f0b19a39a88fe82613a0671768ecd8abded64c2
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2023-09-19 21:33:51 +0100

    Functions with generics.

---
+++ src/04-generics.ts
@@ -0,0 +1,24 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './myTypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+// generic function that returns a sorted copy of an array of any type, where the second parameter is the sorting criterion.
+function sort<T>(data : T[], criteria: (a: T,b: T) => number) : T[] | undefined {
+    return data.sort(criteria)
+}
+
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
